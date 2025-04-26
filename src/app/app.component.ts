0a7c570b5ace394e47482ba0d36ea39a666fe433
franklinmathew <franklinroyson213191@gmail.com>
2025-04-27 03:20:03 +0530

    functionality-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testapp';
+  rectColor = '';
+  rectWidth = 150;
+  rectHeight = 150;
+  rotationAngle = 0;
+
+  changeColor(color: string) {
+    this.rectColor = color;
+  }
+  adjustHeight(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.rectHeight = +input.value;
+  }
+  adjustWidth(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.rectWidth = +input.value;
+  }
+  rotateRectangle(direction: string) {
+    if (direction === 'rightRotation') {
+      this.rotationAngle += 10;
+    } else if (direction === 'leftRotation') {
+      this.rotationAngle -= 10;
+    }
+  }
 }
